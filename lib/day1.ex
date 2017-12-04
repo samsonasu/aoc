f6cabd1735e6462687b77e4203dc581f5d82cc2d
Brian Samson <brian@briansamson.com>
2017-12-04 08:44:36 -0600

    day 1 part2

--- lib/day1.ex
+++ lib/day1.ex
@@ -6,6 +6,17 @@ defmodule Day1 do
     day1_acc([List.first(vals), List.last(vals)], acc)
   end
 
+  def go2 do
+    {:ok, input} = File.read "data/day1.input"
+    part2 input
+  end
+
+  def part2(input) do
+    vals = String.graphemes(input)
+      |> Enum.map(&String.to_integer(&1))
+    part2_acc(vals, 0 , 0)
+  end
+
   defp day1_acc([first | rest], acc) do
     add = case List.first(rest) do
       ^first -> String.to_integer(first)
@@ -16,5 +27,20 @@ defmodule Day1 do
 
   defp day1_acc([], acc) do
     acc
+  end
+
+  defp part2_acc(list, position, acc) do
+    len = length(list)
+    if position <= len do
+      target = Enum.at(list, position)
+      halfway = Enum.at(list, rem(div(len,2)  + position, len))
+      add = case target do
+        ^halfway -> halfway
+        _ -> 0
+      end
+      part2_acc(list, position+1, acc + add)
+    else
+      acc
+    end
   end
 end
